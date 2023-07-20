953bd974490b53c446b97cf0a97d96769f0a8181
Bruno-Warken <bruno.owntec@gmail.com>
2023-07-20 14:54:12 -0300

    cloneStreaming new files commit
    Commit do novo projeto de clone de streaming, desta vez criado utilizando o comando "npm init vue@latest".

---
+++ cloneStreaming/src/App.vue
@@ -0,0 +1,17 @@
+<script>
+
+import {defineComponent} from "vue";
+import CloneMainPage from "@/components/CloneMainPage.vue";
+
+export default defineComponent({
+	components: {CloneMainPage}
+})
+</script>
+
+<template>
+	<CloneMainPage></CloneMainPage>
+</template>
+
+<style>
+
+</style>
---
+++ cloneStreaming/src/components/CloneMainPage.vue
@@ -0,0 +1,15 @@
+﻿<template>
+	
+</template>
+
+<script>
+export default {
+	setup(){
+		
+	}
+}
+</script>
+
+<style>
+
+</style>
